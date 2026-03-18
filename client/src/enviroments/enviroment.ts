36ba4676a030862813baebf4ba1cadec5fddf837
fhidalgoGC <111799320+fhidalgoGC@users.noreply.github.com>
2026-03-18 22:55:01 +0000

    d

--- client/src/enviroments/enviroment.ts
+++ client/src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  rifaId: "590520852520927455638852/LCH01-3887",
-  apiBaseUrl: "https://qtriav9nzf.execute-api.us-east-1.amazonaws.com/dev",
+  rifaId: "LCH01",
+  apiBaseUrl: "https://7cz22z8bee.execute-api.us-east-1.amazonaws.com/dev",
 };
